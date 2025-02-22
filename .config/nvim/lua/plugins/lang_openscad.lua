3880bbb3828d5e6e630f8e9c6bf5194466ee55bb
Vladimir Ovechkin <ovchingus@gmail.com>
2025-02-22 17:16:44 +0300

    chore(openscad-lsp): add openscad-lsp

---
+++ .config/nvim/lua/plugins/lang_openscad.lua
@@ -0,0 +1,20 @@
+return {
+  -- {
+  --   "williamboman/mason.nvim",
+  --   opts = {
+  --     ensure_installed = {
+  --       "openscad-lsp",
+  --       "clang-format",
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "stevearc/conform.nvim",
+  --   -- opts will be merged with the parent spec
+  --   opts = {
+  --     formatters_by_ft = {
+  --       -- openscad = { "prettierd" },
+  --     },
+  --   },
+  -- },
+}
