eb9db10692847318983d4c213de8d8843fb8fbd4
Vladimir Ovechkin <ovchingus@gmail.com>
2025-08-05 15:23:15 +0300

    feat: setup copilot-chat nvim plugin

--- macos/.config/sketchybar/items/widgets/lang.lua
+++ macos/.config/sketchybar/items/widgets/lang.lua
@@ -1,36 +1,38 @@
 local function get_input_source_name()
- -- Run defaults command and capture its output
- local handle = io.popen("defaults read com.apple.HIToolbox.plist AppleCurrentKeyboardLayoutInputSourceID")
- local source = handle:read("*a")
- handle:close()
+	-- Run defaults command and capture its output
+	local handle = io.popen("defaults read com.apple.HIToolbox.plist AppleCurrentKeyboardLayoutInputSourceID")
+	local source = handle:read("*a")
+	handle:close()
 
- -- Remove trailing whitespace/newline
- source = source:gsub("^%s*(.-)%s*$", "%1")
+	-- Remove trailing whitespace/newline
+	source = source:gsub("^%s*(.-)%s*$", "%1")
 
- -- Map input sources to labels
- local input_map = {
-   ["com.apple.keylayout.ABC"] = "en",
-   ["com.apple.keylayout.Russian"] = "ru",
- }
+	-- Map input sources to labels
+	local input_map = {
+		["com.apple.keylayout.ABC"] = "en",
+		["com.apple.keylayout.Russian"] = "ru",
+		["com.apple.keylayout.RussianWin"] = "ru",
+	}
 
- return input_map[source] or source
+	return input_map[source] or source
 end
 
 sbar.add("event", "input.changed", "AppleSelectedInputSourcesChangedNotification")
 
 local input = sbar.add("item", {
- icon = { drawing = false },
- label = get_input_source_name(),
- position = "right",
+	icon = { drawing = false },
+	label = get_input_source_name(),
+	position = "right",
 })
 
 input:subscribe("input.changed", function(_)
- input:set({
-   label = get_input_source_name()
- })
+	input:set({
+		label = get_input_source_name(),
+	})
 end)
 
 input:subscribe("mouse.clicked", function(_)
-  -- use cmd space to change lang
- sbar.exec("osascript -e 'tell application \"System Events\" to key code 49 using command down'")
+	-- use cmd space to change lang
+	sbar.exec("osascript -e 'tell application \"System Events\" to key code 49 using command down'")
 end)
+
